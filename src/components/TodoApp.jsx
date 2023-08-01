5a11781b171d444554e278c02178c0b466ca7817
Yekutiel Katanov <yekutielkatanov@gmail.com>
2023-08-01 23:54:54 +0300

    project 1

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,24 +1,63 @@
-import { useState } from "react"
+import { useState } from "react";
+import Todo from "./todo";
+
+import "./todoApp.css";
 
 function TodoApp() {
   const [title, setTitle] = useState("Hola");
-  function handleClick(e){
-    e.preventDefault();
-    setTitle('Yekutiel')
-  }
-  function handleChange(e){
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(e) {
     const value = e.target.value;
     setTitle(value);
   }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: title,
+      completed: false,
+    };
+    const temp = [...todos];
+    temp.unshift(newTodo);
 
+    setTodos(temp);
+    setTitle("");
+  }
+
+  function handleUpdate(id, value) {
+    const temp = [...todos];
+    const item = temp.find((item) => item.id === id);
+    item.title = value;
+    setTodos(temp);
+  }
+
+  function handleDelete(id) {
+    const temp = todos.filter((item) => item.id !== id);
+    setTodos(temp);
+  }
   return (
     <div className="todoContainer">
-      <form className="todoCreateForm">
-        <input onChange={handleChange} className="todoInput" value={title}/>
-        <input onClick={handleClick} className="todoButtonCreate" type="submit" value="Create todo" />
-       
+      <form className="todoCreateForm" onSubmit={handleSubmit}>
+        <input onChange={handleChange} className="todoInput" value={title} />
+        <input
+          onClick={handleSubmit}
+          className="button"
+          type="submit"
+          value="Create todo"
+        />
       </form>
+      <div className="todosContainer">
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            item={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
-export default TodoApp
+export default TodoApp;
